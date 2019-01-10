3051eaf064d4d0c5b591ac4b239d2e9c969fc526
sajid8212 <sajid8212@gmail.com>
2019-01-10 23:06:45 +0600

    Adder Model & JSONParser

--- app/src/main/java/com/tanvir/tanvirmahmudkhan/myapplication/MainActivity.java
+++ app/src/main/java/com/tanvir/tanvirmahmudkhan/myapplication/MainActivity.java
@@ -5,18 +5,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.tanvir.tanvirmahmudkhan.model.Flower;
+import com.tanvir.tanvirmahmudkhan.parser.FlowerJSONParser;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     TextView tv1;
+    List<Flower> flowers;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         tv1 = findViewById(R.id.tv1);
+        //flowers = new ArrayList<>();
         MyTask myTask = new MyTask();
 
         myTask.execute("http://services.hanselandpetal.com/feeds/flowers.json");
+
+
+
+
 
     }
 
@@ -30,7 +42,21 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(String s) {
-            tv1.setText(s);
+
+            flowers= FlowerJSONParser.parseFeed(s);
+
+            if(flowers != null){
+                for (int i=0; i<flowers.size(); i++) {
+                    String str = tv1.getText().toString();
+
+                    tv1.setText( str + "\n" + flowers.get(i).getName());
+                }
+            }
+            else{
+                tv1.setText("Joy Bangla");
+            }
+
+
         }
 
         @Override
